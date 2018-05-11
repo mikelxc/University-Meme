840c52b350e8671a90f262ddcdfcf51e2ecd43ff
Mike Liu <mike000708@me.com>
2018-05-11 15:55:45 +0800

    initial commit

---
+++ generator.js
@@ -0,0 +1,57 @@
+let topText, bottomText, imageInput, generateBtn, changeBtn, canvas, ctx, date1, dateNow, timeLeft, topDes, middleDes, bottomDes, defaultImg;
+
+function init () {
+  defaultImg = document.getElementById("working-image");
+  topText = document.getElementById("top-text");
+  bottomText = document.getElementById("bottom-text");
+  imageInput = document.getElementById("image-input");
+  generateBtn = document.getElementById("generate");
+  changeBtn = document.getElementById("change");
+  canvas = document.getElementById("meme-canvas");
+
+  ctx = canvas.getContext("2d");
+  canvas.width = canvas.height = 0;
+
+  changeBtn.addEventListener('click', function(){
+    let reader = new FileReader();
+    reader.onload = function () {
+      let img = new Image;
+      img.src = reader.result;
+      generateMeme(img,"","","");
+    };
+    reader.readAsDataURL(imageInput.files[0]);
+    defaultImg.src = reader.result;
+  });
+
+  generateBtn.addEventListener('click', function() {
+    generateMeme(defaultImg, timeLeft, topText.value, bottomText.value);
+  });
+}
+
+function timeCalculation () {
+  date1 = new Date ("June 7, 2018");
+  dateNow = Date.now();
+  timeLeft = parseInt((date1 - dateNow)/(1000*60*60*24));
+  let displayTime = document.getElementById("time-left");
+  displayTime.innerHTML = timeLeft;
+}
+
+function generateMeme (img = defaultImg, topDes = timeLeft, middleDes="的兄弟学校也看不起的", bottomDes="野鸡大学WCU") {
+  canvas.width =  img.width;
+  canvas.height = img.height;
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.drawImage(img,0,0);
+  let fontSize1 = 70/ topDes.length;
+  let fontSize2 = 120/ bottomDes.length;
+  ctx.font =  '44pt "mainFont"';
+  ctx.textBaseline = "top";
+  ctx.fillStyle = "white";
+  ctx.fillText(topDes, 472, 72, 300);
+  ctx.font =  fontSize1 + 'pt Calibri';
+  ctx.fillText(middleDes, 80, 495, 600);
+  ctx.font =  fontSize2 + 'pt Calibri';
+  ctx.fillText(bottomDes, 85, 620, 400);
+}
+
+timeCalculation();
+init();
